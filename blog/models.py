0f297b6c46b2e8cbeb889ba61f81e2d0a229241b
Gary Martin <gary.martin@physics.org>
2016-02-26 00:14:07 +0000

    Adds markdown for posts including data migration.

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.utils import timezone
+import markdown
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    text_markdown = models.TextField(default='')
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+
+    def save(self):
+        self.text = markdown.markdown(self.text_markdown)
+        super(Post, self).save()
 
     def publish(self):
         self.published_date = timezone.now()
