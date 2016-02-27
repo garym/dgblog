fb46b783ed7151bcc591c11e4e1d0f3b9bda04cf
Gary Martin <gary.martin@physics.org>
2016-02-27 01:16:04 +0000

    Add approvable comments

--- blog/models.py
+++ blog/models.py
@@ -8,10 +8,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     text_markdown = models.TextField(default='')
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def save(self):
         self.text = markdown.markdown(self.text_markdown)
@@ -25,5 +23,23 @@ class Post(models.Model):
         self.published_date = None
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name="comments")
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
